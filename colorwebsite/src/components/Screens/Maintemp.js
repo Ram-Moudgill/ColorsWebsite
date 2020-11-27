fcefaa03983928e8c1aaf20b20ed85be727ced0a
ramgill <rammoudgill3054@outlook.com>
2020-11-27 13:39:49 +0530

    adding body color  in Maintemp.js

--- colorwebsite/src/components/Screens/Maintemp.js
+++ colorwebsite/src/components/Screens/Maintemp.js
@@ -51,7 +51,13 @@ const Maintemp = ({ palettefortemp, sidebarstatus }) => {
   }
   return (
     <>
-      <div className='container-fluid p-0 m-0'>
+      <div
+        className='container-fluid p-0 m-0'
+        style={{
+          background: color2 ? color2 : '#fff',
+          position: 'sticky',
+        }}
+      >
         <div className='row p-0 m-0'>
           <div
             className=' col-12 m-0 p-0'
@@ -153,7 +159,7 @@ const Maintemp = ({ palettefortemp, sidebarstatus }) => {
                 <path
                   d='M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z'
                   className='shape-fill'
-                  fill={'#fff'}
+                  fill={color2 ? color2 : '#fff'}
                 ></path>
               </svg>
             </div>
